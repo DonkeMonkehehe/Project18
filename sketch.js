5c781774deefd18b38d02becd2e532f8f300b4d0
DonkeMonkehehe <99879215+DonkeMonkehehe@users.noreply.github.com>
2022-06-26 17:39:14 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,14 @@
+var s1
+
+
+function setup() {
+  createCanvas(400,400);
+  s1=new Student("bob",15,10);
+  s1.display();
+}
+
+function draw() 
+{
+  background(30);
+  
+}
---
+++ student.js
@@ -0,0 +1,20 @@
+
+class Student
+{
+   //name,age,grade
+   constructor(n,a,g){
+       this.name=n;
+       this.age=a;
+       this.grade=g;
+   }
+   
+ display(){
+     console.log(this.name,this.age,this.grade)
+
+}
+
+}
+
+
+
+
